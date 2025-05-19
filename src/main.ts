513e553e184c941597e0d94d924f7600e0e7b945
yjgaia <yjdev88@gmail.com>
2025-05-19 17:25:38 +0900

    feat: integrate MaterialLoadingSpinner into AppCompConfig and add URLPattern polyfill

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import { Router } from "@commonmodule/app";
 import { AppComponents } from "@commonmodule/component-showcase";
+import { AppCompConfig } from "@commonmodule/app-components";
+import { MaterialLoadingSpinner } from "@commonmodule/material-loading-spinner";
+
+AppCompConfig.LoadingSpinner = MaterialLoadingSpinner;
 
 Router
   .add("/app-components", AppComponents);
